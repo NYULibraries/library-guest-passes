bc3321dbaec7e08dc9ca4721ee42286fc779ff0b
laurxnemeth <lauranemeth.pr@gmail.com>
2021-05-17 14:11:07 -0400

    add input fields

--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,16 @@
 import React,{ useState } from 'react';
 
 const Form = () =>{
-  const [name,setName] = useState('');
-  const [guest,setGuest] = useState('');
+  const [name, setName] = useState('');
+  const [guest, setGuest] = useState('');
+  const [initials, setInitials] = useState('');
+  const [typeOfId, setTypeOfId] = useState('');
+  const [notes, setNotes] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault() 
-    console.log(); 
+    const data = [name, guest, initials, typeOfId, notes]
+    console.log(data); 
   }
   const handleChange = (e, type) => {
     let updateFn;
@@ -17,13 +21,28 @@ const Form = () =>{
       case 'guest':
         updateFn = setGuest;
         break;
+      case 'initials':
+        updateFn = setInitials;
+        break;
+        case 'typeOfId':
+          updateFn = setTypeOfId;
+          break;
+        case 'notes':
+          updateFn = setNotes;
+          break;
+        default:
+          return;
     }
+    updateFn(e.currentTarget.value);
   }
 
     return (
         <form onSubmit={handleSubmit}>
           <input placeholder="Name" value={name} onChange={(e) => handleChange(e, "name")} /> 
           <input placeholder="Guest" value={guest} onChange={(e) => handleChange(e, "guest")} />
+          <input placeholder="Employee Initials" value={initials} onChange={(e) => handleChange(e, "initials")} />
+          <input placeholder="ID Type" value={typeOfId} onChange={(e) => handleChange(e, "typeOfId")} />
+          <input placeholder="notes" value={notes} onChange={(e) => handleChange(e, "notes")} />
           <button>Submit</button>
        </form>
     )
