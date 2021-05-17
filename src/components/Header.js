0f599b759286a4a6f705c4b2dd0dec74ed95def5
laurxnemeth <lauranemeth.pr@gmail.com>
2021-05-17 16:36:27 -0400

    looks acceptable now

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div>
+    <div id='header'>
       <h1>Library Privileges</h1>
       <h3>Passes Form</h3>
       <hr />
