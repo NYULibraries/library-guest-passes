086900ec817a429c14c5cfa907dfb62b38041590
laurxnemeth <lauranemeth.pr@gmail.com>
2021-05-25 11:42:29 -0400

    database runs

--- server/app.js
+++ server/app.js
@@ -1,10 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//create connection
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123456789'
+});
+
+// Connect
+db.connect((err) => {
+  if(err) throw err;
+  console.log('MySql Connected...');
+});
+
+// Create DB
+// app.get('/createdb', (req, res) => {
+//   let sql = 'CREATE DATABASE nodemysql';
+//   db.query(sql, (err, result) => {
+//       if(err) throw err;
+//       console.log(result);
+//       res.send('Database created...');
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
