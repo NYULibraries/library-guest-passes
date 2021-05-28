2115a5c1079d1b4601903f6622f4b35ebb40c0a3
laurxnemeth <lauranemeth.pr@gmail.com>
2021-05-28 14:37:34 -0400

    sequelize attempts

--- server/db/database.js
+++ server/db/database.js
@@ -1,6 +1,13 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(
-  'mysql',
-  
-)
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'mysql',
+  dialect: 'mysql',
+  user: 'root',
+  password: ''
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('connected to the db')
+  })
