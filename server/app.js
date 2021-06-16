8cb35f966ce44f43583ac1a494d1f42be51174a7
laurxnemeth <lauranemeth.pr@gmail.com>
2021-06-16 16:33:50 -0400

    express app is alive

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-const port = 5000
+const PORT = 5000;
+const HOST = '0.0.0.0';
+//const routes = require('./routes');
 
 const db = mysql.createConnection({
   host: 'db',
@@ -15,7 +17,8 @@ db.connect((err) => {
   console.log('MySql Connected...');
 });
 
+app.use(express.json())
+app.use('/', routes);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
