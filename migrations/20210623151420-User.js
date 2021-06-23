07f9244ee6000536e553ccf3ad5cef713e54fa70
laurxnemeth <lauranemeth.pr@gmail.com>
2021-06-23 16:09:57 -0400

    updated models

---
+++ migrations/20210623151420-User.js
@@ -0,0 +1,61 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id:{
+        type: Sequelize.INTEGER,
+        autoIncrement:true,
+        allowNull:false,
+        primaryKey:true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      guest: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      initials: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      restrictions: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      idtype: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      cardissue: {
+        type: Sequelize.DATE,
+        allowNull:false
+      },
+      cardexp: {
+        type: Sequelize.DATE,
+        allowNull:false
+      },
+      userStatus: {
+        type: Sequelize.STRING,
+        allowNull:false,
+        defaultValue: 1
+      },
+      loginCount: {
+        type: Sequelize.TINYINT,
+        allowNull:false,
+        defaultValue: 1
+      },
+      notes: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
