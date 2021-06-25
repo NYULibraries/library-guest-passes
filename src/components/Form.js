b843e273ac27a5b06140e4101e8cb234124eae06
laurxnemeth <lauranemeth.pr@gmail.com>
2021-06-25 13:36:30 -0400

    tab back

--- src/components/Form.js
+++ src/components/Form.js
@@ -51,69 +51,36 @@ const Form = () =>{
     }
   }
 
-  const handleChange = (e, type) => {
-    let updateFn;
-    switch (type){
-      case 'name':
-        updateFn = setName;
-        break;
-      case 'initials':
-        updateFn = setInitials;
-        break;
-      case 'typeOfId':
-        updateFn = setTypeOfId;
-        break;
-      case 'restrictions':
-        updateFn = setRestrictions;
-        break;
-      case 'status':
-        updateFn = setStatus;
-        break;
-      case 'issuedOn':
-        updateFn = setIssuedOn;
-        break;
-      case 'expiresOn':
-        updateFn = setExpiresOn;
-        break;
-      case 'notes':
-        updateFn = setNotes;
-        break;
-      default:
-        return;
-    }
-    updateFn(e.currentTarget.value);
-  }
-
-    return (
-        <form data-testid='passes-form' onSubmit={handleSubmit}>
-          <label htmlFor='name'>Name</label>
-          <input id='name' value={name} onChange={(e) => handleChange(e, "name")} /> 
-          <label htmlFor='permission'>Permission status</label>
-          <p id='permission'>{permission}</p>
-          <label htmlFor='employee_initials'>Employee Initials</label>
-          <input data-testid='form-input' id="employee_initials" value={initials} onChange={(e) => handleChange(e, "initials")} />
-          <label htmlFor='id_type'>ID Type</label>
-          <input data-testid='form-input' id="id_type" value={typeOfId} onChange={(e) => handleChange(e, "typeOfId")} />
-          <label htmlFor='restrictions'>Restrictions</label>
-          <select id='restrictions' value={restrictions} onChange={(e) => handleChange(e, "restrictions")}>
-            {restrictionList.map(e => <option value={e} key={e}>{e}</option>)}
-          </select>
-          <label htmlFor='status'>Status</label>
-          <select id='status' value={status} onChange={(e) => handleChange(e, 'status')}>
-            {statusList.map(e => <option value={e} key={e}>{e}</option>)}
-          </select>
-          <label htmlFor='issued_on'>Card Issued On</label>
-          <input data-testid='form-input' id='issued_on' type='date' value={issuedOn} onChange={(e) => handleChange(e, 'issuedOn')} />
-          <label htmlFor='expires_on'>Expiration Date</label>
-          <input data-testid='form-input' id='expires_on' type='date' value={expiresOn} onChange={(e) => handleChange(e, 'expiresOn')} />
-          <label htmlFor='notes'>Notes</label>
-          <input data-testid='form-input' id='notes' value={notes} onChange={(e) => handleChange(e, 'notes')} />
-          <button>Submit</button>
-          <div className='msgWrap'>
-            <span id='errorMsg'>{errorMsg}</span>
-          </div>
-       </form>
-    )
+  return (
+      <form data-testid='passes-form' onSubmit={handleSubmit}>
+        <label htmlFor='name'>Name</label>
+        <input id='name' value={name} onChange={(e) => handleChange(e, "name")} /> 
+        <label htmlFor='permission'>Permission status</label>
+        <p id='permission'>{permission}</p>
+        <label htmlFor='employee_initials'>Employee Initials</label>
+        <input data-testid='form-input' id="employee_initials" value={initials} onChange={(e) => handleChange(e, "initials")} />
+        <label htmlFor='id_type'>ID Type</label>
+        <input data-testid='form-input' id="id_type" value={typeOfId} onChange={(e) => handleChange(e, "typeOfId")} />
+        <label htmlFor='restrictions'>Restrictions</label>
+        <select id='restrictions' value={restrictions} onChange={(e) => handleChange(e, "restrictions")}>
+          {restrictionList.map(e => <option value={e} key={e}>{e}</option>)}
+        </select>
+        <label htmlFor='status'>Status</label>
+        <select id='status' value={status} onChange={(e) => handleChange(e, 'status')}>
+          {statusList.map(e => <option value={e} key={e}>{e}</option>)}
+        </select>
+        <label htmlFor='issued_on'>Card Issued On</label>
+        <input data-testid='form-input' id='issued_on' type='date' value={issuedOn} onChange={(e) => handleChange(e, 'issuedOn')} />
+        <label htmlFor='expires_on'>Expiration Date</label>
+        <input data-testid='form-input' id='expires_on' type='date' value={expiresOn} onChange={(e) => handleChange(e, 'expiresOn')} />
+        <label htmlFor='notes'>Notes</label>
+        <input data-testid='form-input' id='notes' value={notes} onChange={(e) => handleChange(e, 'notes')} />
+        <button>Submit</button>
+        <div className='msgWrap'>
+          <span id='errorMsg'>{errorMsg}</span>
+        </div>
+      </form>
+  )
 }
 
 export default Form;
