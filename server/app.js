1608c62ad3aef67ccda8bcc7101a181c14b946bb
Eric Griffis <eric.griffis@nyu.edu>
2021-07-19 14:47:42 -0400

    Make host configurable

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const HOST = '0.0.0.0';
+const HOST = process.env.HOST || '0.0.0.0';
 const routes = require('./routes');
 const cors = require('cors');
 
