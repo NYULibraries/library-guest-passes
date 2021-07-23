c4f3c616a42188cb56747db3442ec18de8918a40
Eric Griffis <eric.griffis@nyu.edu>
2021-07-23 12:53:25 -0400

    Add healthcheck endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const controllers = require('../controllers');
 const router = Router();
 
 router.get('/', (req, res) => res.send('This is root!'))
+router.get('/healthcheck', (req, res) => res.send('{"healthy":true}'))
 
 router.post('/users', controllers.updateOrCreateUser)
 
