f4a9ee6ce0cea501f5e005478b1706de23450e7a
laurxnemeth <lauranemeth.pr@gmail.com>
2021-08-04 10:54:32 -0400

    header img

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 export default function Header() {
   return (
     <div id='header'>
+      <img src="https://cdn.library.nyu.edu/images/nyulibraries-logo.svg" alt="NYU Libraries logo" width="233" height="30" />
       <h1>Library Privileges</h1>
       <h3>Passes Form</h3>
       <hr />
