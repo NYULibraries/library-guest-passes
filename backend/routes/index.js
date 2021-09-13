1f8ef26373ff3fa50e980b86372acca21c708988
laurxnemeth <lauranemeth.pr@gmail.com>
2021-09-13 15:19:55 -0400

    drafting backend routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,15 @@ const controllers = require("../controllers");
 const router = Router();
 
 router.get("/guest", controllers.lookupGuest);
+// router.get("/affiliate", controllers.lookupGuest);
+// router.get("/guests");
+// router.get("/affiliates");
+// router.put("/guests/:id");
+// router.put("/affiliate/:id");
+// router.delete("/guests/:id");
+// router.delete("/affiliates/:id");
 
 router.post("/visit", controllers.createVisit);
+
 
 module.exports = router;
