ed2b95e645287aca6b1b91a6a2b2b7c08c38f564
laurxnemeth <lauranemeth.pr@gmail.com>
2021-09-29 17:21:56 -0400

    front end deleting of previous visits

--- src/components/PreviousVisits.js
+++ src/components/PreviousVisits.js
@@ -38,51 +38,49 @@ function PreviousVisits(props) {
   return (
     <div className="container">
       <h1>Visits</h1>
-      { data.permission_status ? <div><h2>{data.name}</h2><h3>Permission Status: {data.permission_status.toString()}</h3></div> : <div></div>}
-      <table className="table table-hover table-bordered">
-        <thead>
-        <tr>
-          <th scope="col">Card Issued on:</th>
-          <th scope="col">Card Expires on:</th>
-          <th scope="col">Staff Initials</th>
-          <th scope="col">Type of ID</th>
-          <th scope="col">Restrictions</th>
-          <th scope="col">Status</th>
-          <th scope="col">Notes</th>
-        </tr>
-        </thead>
-        <tbody>
-          {
-            data.Visits ? data.Visits.map((e) => (
-              <tr key={e.id}>
-                <td>{new Date(e.cardissue).toDateString()}</td>
-                <td>{new Date(e.cardexp).toDateString()}</td>
-                <td>{e.initials}</td>
-                <td>{e.idtype}</td>
-                <td>{e.restrictions}</td>
-                <td>{e.status}</td>
-                <td>{e.notes}</td>
-                <td>
-                  <div className="btn-group mb-3">
-                    <button className="btn btn-outline-secondary">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye" viewBox="0 0 16 16">
-                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
-                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
-                      </svg>
-                    </button>
-                    <button className="btn btn-outline-secondary" type="button" onClick={() => handleDelete(backendDomain, e.id)} >
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                        <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                      </svg>
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            )) : <div></div>
-          }
-        </tbody>
-      </table>
+      { data && data.hasOwnProperty("permission_status") ? <div><h2>{data.name}</h2><h3>Permission Status: {data.permission_status.toString()}</h3></div> : <div></div>}
+      { data && data.hasOwnProperty("Visits") && data.Visits.length ?
+        <table className="table table-hover table-bordered">
+          <thead>
+          <tr>
+            <th scope="col">Card Issued on:</th>
+            <th scope="col">Card Expires on:</th>
+            <th scope="col">Staff Initials</th>
+            <th scope="col">Type of ID</th>
+            <th scope="col">Restrictions</th>
+            <th scope="col">Status</th>
+            <th scope="col">Notes</th>
+            <th scope="col">Delete</th>
+          </tr>
+          </thead>
+          <tbody>
+            {
+              data && data.hasOwnProperty("Visits") && data.Visits.length ? data.Visits.map((e) => (
+                <tr key={e.id}>
+                  {console.log(data.Visits.length, data.hasOwnProperty("Visits"))}
+                  <td>{new Date(e.cardissue).toDateString()}</td>
+                  <td>{new Date(e.cardexp).toDateString()}</td>
+                  <td>{e.initials}</td>
+                  <td>{e.idtype}</td>
+                  <td>{e.restrictions}</td>
+                  <td>{e.status}</td>
+                  <td>{e.notes}</td>
+                  <td>
+                    <div className="">
+                      <button className="btn btn-outline-secondary" type="button" onClick={() => handleDelete(backendDomain, e.id)} >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                          <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                        </svg>
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              )) : <div></div>
+            }
+          </tbody>
+        </table> : <div><h1>No Visits Recorded</h1></div>
+      }
     </div>
   );
 
