64667d1cb3c82d97c476a4ba50376438d49c9232
laurxnemeth <lauranemeth.pr@gmail.com>
2021-10-12 16:02:34 -0400

    change to modal view

--- src/components/EditVisitor.js
+++ src/components/EditVisitor.js
@@ -5,9 +5,13 @@ const backendDomain = `${
 }`;
 
 const EditVisitor = (props) =>{
-  const visitorObject = props.location.state; 
+  const visitorObject = props.state
   const [name, setName] = useState("");
   const [permissionStatus, setPermissionStatus] = useState();
+
+  const onClose = (e) => {
+    props.onClose && props.onClose(e);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault() 
@@ -25,19 +29,23 @@ const EditVisitor = (props) =>{
       .then(res => console.log(res))
   }
 
-    return (
-        <div>
-          { 
-            visitorObject.hasOwnProperty('name') ? 
-            <form onSubmit={handleSubmit}>
+  if(!props.show){return null}
+  return (
+      <div>
+        { 
+          visitorObject && visitorObject.hasOwnProperty('name') ? 
+          <form onSubmit={handleSubmit}>
             <input placeholder="Name" name="name" defaultValue={visitorObject.name} onChange={(e) => setName(e.target.value)} /> 
             <input placeholder="Permission Status" name="permission_status" defaultValue={visitorObject.permission_status} onChange={(e) => setPermissionStatus(e.target.value)} />
             <button>Submit</button>
-            </form> : <div></div>
-          }
+          </form> : <div><h1>Hello!!</h1></div>
+        }
+        <div>
+          <button className="toggle-button" onClick={onClose}> Close </button>
         </div>
-       
-    )
+      </div>
+      
+  )
 }
 
 export default EditVisitor;
--- src/components/Guest.js
+++ src/components/Guest.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
+import EditVisitor from './EditVisitor';
 
 const backendDomain = `${
   process.env.REACT_APP_BACKEND_FULL_HOST || "http://localhost:5000"
@@ -7,12 +8,14 @@ const backendDomain = `${
 
 function Guest() {
   const [data, setData] = useState([]);
+  const [show, setShow] = useState(false);
+  const [visitorInfo, setVisitorInfo] = useState({})
 
   const fetchGuests = () => {
     fetch(`${backendDomain}/guests`)
       .then(res => res.json())
       .then(json => setData(json));
-  }
+  };
 
   useEffect(() => {
     fetchGuests();
@@ -35,6 +38,11 @@ function Guest() {
     setData(guests)
   }
 
+  const showModal = (e)=> {
+    setShow(!show);
+    setVisitorInfo(e);
+  }
+
   return (
     <div className="container">
       <h1>Guests</h1>
@@ -54,13 +62,11 @@ function Guest() {
                 <td>{e.permission_status.toString()}</td>
                 <td>
                   <div className="btn-group mb-3">
-                    <Link to={{pathname: `/admin/guests/${e.id}/edit`, state:{"name": e.name, "permission_status":e.permission_status, "typeOfVisitor": "guests", "id": e.id} }}>
-                      <button className="btn btn-outline-secondary" type="button">
+                      <button className="btn btn-outline-secondary" type="button" value={e} onClick={()=> { showModal(e) }}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
                           <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                         </svg>
                       </button>
-                    </Link>
                     <Link to={{pathname: `/admin/guests/${e.id}`}}>
                       <button className="btn btn-outline-secondary" type="button">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye" viewBox="0 0 16 16">
@@ -82,6 +88,7 @@ function Guest() {
           }
         </tbody>
       </table>
+      <EditVisitor show={show} state={visitorInfo} onClose={showModal} />
     </div>
   );
 
