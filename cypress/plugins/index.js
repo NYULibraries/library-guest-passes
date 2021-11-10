d6102be8d2fc910bea5ebc909dfe60805a27faab
laurxnemeth <lauranemeth.pr@gmail.com>
2021-11-10 15:16:17 -0500

    add configurations

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,7 +16,9 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
+const injectDevServer = require("@cypress/react/plugins/react-scripts");
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+  injectDevServer(on, config);
+  return config;
+};
