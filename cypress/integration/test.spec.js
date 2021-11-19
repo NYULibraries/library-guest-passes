5f81f3b4391d13c7db1bd2efb2add8749e12c786
laurxnemeth <lauranemeth.pr@gmail.com>
2021-11-19 10:45:56 -0500

    progress in cypress

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,11 +1,14 @@
 describe('The Home Page', function () {
+  before(() => {
+    cy.visit('/')
+  })
+
   describe('My First Test', () => {
     it('Does not do much!', () => {
       expect(true).to.equal(true)
     })
 
     it('successfully loads', function () {
-      cy.visit('/')
       cy.contains('type')
     })
   })
