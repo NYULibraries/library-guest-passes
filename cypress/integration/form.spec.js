6ef71f41d0ab227cab1abe3f3a386089b548ae63
laurxnemeth <lauranemeth.pr@gmail.com>
2021-11-22 11:48:51 -0500

    start developing tests

---
+++ cypress/integration/form.spec.js
@@ -0,0 +1,19 @@
+describe('The Home Page', function () {
+  before(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  describe('Form Elements', () => {
+    it('successfully loads', function () {
+      cy.get('form').contains('Submit');
+      cy.get('form').contains('Clear');
+    })
+  })
+
+  describe('Submit', () => {
+    it('returns error message on incomplete form', function(){
+      cy.get('button.btn-primary').click();
+      cy.get('msgWrap').contains('Oops!');
+    })
+  })
+})  
