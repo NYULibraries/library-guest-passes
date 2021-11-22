bea5af34eb74ba1bef017305736deeabf10d981b
laurxnemeth <lauranemeth.pr@gmail.com>
2021-11-22 13:57:34 -0500

    work in progress form specs

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -11,12 +11,35 @@ describe('The Home Page', function () {
   })
 
   describe('Submit', () => {
+    it('successfully submits form if all necessary fields are filled', function(){
+      cy.get('#guest_name')
+        .should('be.visible')
+        .type('John Doe')
+      cy.get('#affiliate_name')
+        .should('be.visible')
+        .type('Joanne Doe')
+      cy.get('#initials')
+        .should('be.visible')
+        .type('LN')
+      cy.get('#idtype')
+        .should('be.visible')
+        .type('passport')
+      cy.get('#restrictions')
+        .should('be.visible')
+        .select('Visual History Archives')
+      cy.get('#status')
+        .should('be.visible')
+        .select('Sponsored Access')
+      // cy.get('button[type="submit"]')
+      //   .should('be.visible')
+      //   .click()
+  
+      cy.contains('You will receive an email to finish the purchase.')
+        .should('be.visible')
+    })
     it('returns error message on incomplete form', function(){
       cy.get('button.btn-primary').click();
       cy.get('div.msgWrap > em').contains('Oops!');
-    })
-    it('successfully submits form if all necessary fields are filled', function(){
-      
     })
   })
 })  
